1a46e701f8b6ea0cf87d7ab71faa68cf920d09fd
Alexey <72343402+Mor-ald@users.noreply.github.com>
2021-09-18 13:44:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ with open("dataArticle.csv", "w") as csvfile:
         ["title", "href", "annotation"]
     )
 
-url = 'https://habr.com/ru/search/page1/?q=jamstack&target_type=posts&order=relevance'
+url = "https://habr.com/ru/search/page1/?q=jamstack&target_type=posts&order=relevance"
 i = 1
 while (i < 4):
     html = urlopen(url)
     bs = BeautifulSoup(html, "html.parser") 
-    nameList = bs.findAll('h2', {'class': 'tm-article-snippet__title'})
-    hrefList = bs.findAll('a', {'class': 'tm-article-snippet__title-link'})
-    annotationList = bs.findAll('div', {'class': 'article-formatted-body'})
+    nameList = bs.findAll("h2", {"class": "tm-article-snippet__title"})
+    hrefList = bs.findAll("a", {"class": "tm-article-snippet__title-link"})
+    annotationList = bs.findAll("div", {"class": "article-formatted-body"})
     for j in range(len(nameList)):
         name = nameList[j]
         href = hrefList[j]
@@ -24,7 +24,7 @@ while (i < 4):
         with open("dataArticle.csv", "a") as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(
-                [name.get_text(), "https://habr.com"+href.get('href'), ann.get_text()]
+                [name.get_text(), "https://habr.com"+href.get("href"), ann.get_text()]
             )
     url = url.replace(str(i), str(i+1))
     i += 1
